34b35a4a27d00031c19bf97679f7b47be87bef0a
zhangwei <zhangwei@36kr.com>
2016-01-14 19:24:12 +0800

    init

---
+++ src/main/java/com/online/exams/system/webapp/config/boot.java
@@ -0,0 +1,7 @@
+package com.online.exams.system.webapp.config;
+
+/**
+ * Created by 36kr on 16/1/13.
+ */
+public class boot {
+}
---
+++ src/main/java/com/online/exams/system/webapp/controller/login/LoginController.java
@@ -0,0 +1,7 @@
+package com.online.exams.system.webapp.controller.login;
+
+/**
+ * Created by 36kr on 16/1/13.
+ */
+public class LoginController {
+}
